57fde4ba3ddf2f29eeb8bde2850ef797245686ea
davidekete <eketedavid@gmail.com>
2024-02-26 14:36:02 +0100

    refactor: create strategy file

--- main.js
+++ main.js
@@ -4,11 +4,9 @@ const app = express();
 const port = 3000;
 const expressSession = require("express-session");
 const authRouter = require("./src/authRoutes");
-const passportCustom = require("passport-custom");
-const StytchStrategy = passportCustom.Strategy;
 const passport = require("passport");
-const { authenticateSessionJWT } = require("./src/stytch-auth");
 const cookieParser = require("cookie-parser");
+const StytchStrategy = require("./src/stytch-strategy");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,31 +23,7 @@ app.use(
 );
 
 // Define the Stytch strategy
-passport.use(
-  "stytch-strategy",
-  new StytchStrategy(async function (req, done) {
-    try {
-      //try to retrieve sessionToken from the session object
-      const sessionJWT =
-        req.session?.session_jwt || req.session?.passport?.user?.session_jwt;
-
-      // If session token is missing, return an error indicating invalid or missing session
-      if (!sessionJWT) {
-        return done(null, false, {
-          message: "Session token is missing. Please Login",
-        });
-      }
-
-      //Authenticate the session token
-      const response = await authenticateSessionJWT(sessionJWT);
-
-      // If the session token is valid, return the user data
-      return done(null, response);
-    } catch (error) {
-      return done(null, false, { message: "Invalid session", error });
-    }
-  })
-);
+passport.use("stytch-strategy", StytchStrategy);
 
 // Initialize passport
 app.use(passport.initialize());
---
+++ src/stytch-strategy.js
@@ -0,0 +1,29 @@
+const passportCustom = require("passport-custom");
+const CustomStrategy = passportCustom.Strategy;
+const { authenticateSessionJWT } = require("./stytch-auth");
+
+
+const StytchStrategy = new CustomStrategy(async function (req, done) {
+  try {
+    //try to retrieve sessionToken from the session object
+    const sessionJWT =
+      req.session?.session_jwt || req.session?.passport?.user?.session_jwt;
+
+    // If session token is missing, return an error indicating invalid or missing session
+    if (!sessionJWT) {
+      return done(null, false, {
+        message: "Session token is missing. Please Login",
+      });
+    }
+
+    //Authenticate the session token
+    const response = await authenticateSessionJWT(sessionJWT);
+
+    // If the session token is valid, return the user data
+    return done(null, response);
+  } catch (error) {
+    return done(null, false, { message: "Invalid session", error });
+  }
+});
+
+module.exports = StytchStrategy;
